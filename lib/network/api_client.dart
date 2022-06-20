50ab6d6e8657d015ab6aca239df6f344d0c8dce1
Andrew <andrew.neroznak@keepinmind.info>
2022-06-20 20:31:00 +0200

    create api client

---
+++ lib/network/api_client.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/io_client.dart';
+import 'package:weather_app/models/forecast.dart';
+import 'package:weather_app/network/config.dart';
+import 'package:weather_app/network/interceptor_mixin.dart';
+
+class ApiClient with Interceptor {
+  final _Client _client = _Client();
+  Future<Forecast> fetchWeather(String city) {
+    final Map<String, String> params = {
+      'q': city,
+      'appid': Config.apiKey,
+    };
+    final Uri uri = Uri.https(Config.host, '/data/2.5/forecast', params);
+
+    return _client.get(uri).then(interceptor).then((response) {
+      final decoded = json.decode(response.body);
+      return Forecast.fromJson(decoded);
+    });
+  }
+}
+
+class _Client extends IOClient {}
