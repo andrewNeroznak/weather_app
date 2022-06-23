c729fc36269996e477a2bc1e19dde10fef641cb2
Andrew <andrew.neroznak@gmail.com>
2022-06-23 22:05:52 +0200

    fetch saved theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weather_app/network/config.dart';
 import 'package:weather_app/weather_app.dart';
 
-void main() {
-  runApp(const WeatherApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final brightness = SchedulerBinding.instance.window.platformBrightness;
+
+  final pref = await SharedPreferences.getInstance();
+
+  final selectedThemeMode = ThemeModeValue.values.firstWhereOrNull(
+    (element) => element.name == pref.getString(Config.themeModePrefKey),
+  );
+
+  runApp(
+    WeatherApp(
+        themeModeProvider: ThemeModeProvider(
+      selectedThemeMode ??
+          (brightness == Brightness.dark
+              ? ThemeModeValue.dark
+              : ThemeModeValue.light),
+    )),
+  );
 }
